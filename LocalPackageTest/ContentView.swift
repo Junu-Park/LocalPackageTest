19bba1c49bb443ee4c6e532f46121a16c048ac73
Park Junwoo <ays99111@naver.com>
2025-05-11 23:39:31 +0900

    Initial Commit

---
+++ LocalPackageTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalPackageTest
+//
+//  Created by 박준우 on 5/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalPackageTest/LocalPackageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalPackageTestApp.swift
+//  LocalPackageTest
+//
+//  Created by 박준우 on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalPackageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
